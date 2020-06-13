844936dcccc6517bdeb184e770f31dca851acc21
JasonZhongZexin <samsung@svf-tester.asia-east2-a.c.v2ray-project-267121.internal>
2020-06-13 12:15:15 +0000

    add llvm_install script

---
+++ llvm_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+SVFHOME=$(pwd)
+sysOS=`uname -s`
+MacLLVM="https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-apple-darwin.tar.xz"
+UbuntuLLVM="https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz"
+LLVMHome="llvm-10.0.0.obj"
+
+########
+# Download LLVM binary
+########
+if [[ $sysOS == "Darwin" ]]
+then
+       if [ ! -d "$LLVMHome" ]
+       then
+       		echo 'Downloading LLVM binary for MacOS '
+      		curl -L $MacLLVM > llvm-mac.tar.xz
+      	 	mkdir ./$LLVMHome && tar -xf "llvm-mac.tar.xz" -C ./$LLVMHome --strip-components 1
+			echo $(pwd)"/"$LLVMHome
+		rm llvm-mac.tar.xz
+       fi
+elif [[ $sysOS == "Linux" ]]
+then
+       if [ ! -d "$LLVMHome" ]
+       then
+       		echo 'Downloading LLVM binary for Ubuntu'
+      		wget -c $UbuntuLLVM -O llvm-ubuntu.tar.xz
+      		mkdir ./$LLVMHome && tar -xf "llvm-ubuntu.tar.xz" -C ./$LLVMHome --strip-components 1
+			echo $(pwd)"/"$LLVMHome
+		rm llvm-ubuntu.tar.xz
+       fi
+else
+	echo 'not support builds in OS other than Ubuntu and Mac'
+fi
+export LLVM_DIR=$SVFHOME/$LLVMHome
