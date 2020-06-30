a6b45d87a4e59d54a2ffce88132226442485e03a
JasonZhongZexin <samsung@svf-tester.asia-east2-a.c.v2ray-project-267121.internal>
2020-06-30 21:12:41 +0000

    update env script

--- env.sh
+++ env.sh
@@ -1,7 +1,9 @@
 sysOS=`uname -s`
 LLVMHome="llvm-10.0.0.obj"
+script_path=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+cd $script_path
 cd ..
-install_path=$(dirname $(readlink -f $0))
+install_path=$(pwd)
 export LLVM_DIR=$install_path/$LLVMHome
 export PATH=$LLVM_DIR/bin:$PATH
 if [[ $sysOS == "Darwin" ]]
