30d8a61f46ac89fd97d5d607c9eb1167b130d6d5
guanqin-123 <charles.guanqin@gmail.com>
2021-09-07 07:45:04 +1000

    add z3 binary install

--- llvm_install.sh
+++ llvm_install.sh
@@ -3,6 +3,9 @@ SVFHOME=$(pwd)
 sysOS=`uname -s`
 MacLLVM="https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-apple-darwin.tar.xz"
 UbuntuLLVM="https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz"
+MacZ3="https://github.com/Z3Prover/z3/releases/download/z3-4.8.8/z3-4.8.8-x64-osx-10.14.6.zip"
+UbuntuZ3="https://github.com/Z3Prover/z3/releases/download/z3-4.8.8/z3-4.8.8-x64-ubuntu-16.04.zip"
+Z3Home="z3.obj"
 LLVMHome="llvm-12.0.0.obj"
 cd $SVFHOME
 cd ..
@@ -33,9 +36,38 @@ then
 		rm llvm-ubuntu.tar.xz
        fi
 else
-	echo 'not support builds in OS other than Ubuntu and Mac'
+	echo 'not support llvm builds in OS other than Ubuntu and Mac'
 fi
 echo "LLVM_DIR=$install_path/$LLVMHome"
+########
+# Download z3 binary
+########
+if [[ $sysOS == "Darwin" ]]
+then
+       if [ ! -d "$install_path/$Z3Home" ]
+       then
+       		echo 'Downloading z3 binary for MacOS '
+      		curl -L $MacZ3 > z3.zip
+      	 	mkdir $install_path/$Z3Home 
+		echo 'Unzipping z3 binary for MacOS '
+        unzip -q "z3.zip" && mv ./z3-* $install_path/$Z3Home
+		rm z3.zip
+       fi
+elif [[ $sysOS == "Linux" ]]
+then
+       if [ ! -d "$install_path/$Z3Home" ]
+       then
+       		echo 'Downloading z3 binary for Ubuntu'
+      		wget -c $UbuntuZ3 -O z3.zip
+      		mkdir $install_path/$Z3Home 
+		echo 'Unzipping z3 binary for Ubuntu'
+        unzip -q "z3.zip" && mv ./z3-* $install_path/$Z3Home
+		rm z3.zip
+       fi
+else
+	echo 'not support z3 builds in OS other than Ubuntu and Mac'
+fi
+echo "Z3_DIR=$install_path/$Z3Home"
 if [[ $sysOS == "Darwin" ]]
 then 
 ln -s $install_path/svf-lib/SVF-osx $install_path/SVF
